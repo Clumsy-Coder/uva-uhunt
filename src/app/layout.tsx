66c9841e429880cfec9733e7b41926253a83a9de
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2023-11-04 12:08:01 -0600

    chore(theme): add `ThemeProvider` to NextJS app
      ## what
      - add `ThemeProvider` to NextJS app
    
      ## how
      - check
        - https://ui.shadcn.com/docs/dark-mode/next#wrap-your-root-layout
    
      ## why
      - this will enable the usage of darkmode
    
      ## where
      - ./src/app/layout.tsx
    
      ## usage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
 
-import './globals.css'
+import "./globals.css";
 import { cn } from "@/lib/utils";
 
 const inter = FontSans({
@@ -28,7 +29,14 @@ export default function RootLayout({
           inter.variable,
         )}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
