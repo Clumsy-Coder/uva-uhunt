3932cfda19137cee9a72596a3e697fd8d057e135
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2023-12-26 20:07:07 -0700

    feat(api): add some basic checks for endpoint `/api/problems/:problemNum`
      ## what
      - add some basic checks for endpoint `/api/problems/:problemNum`
        - check if param is a number
        - check if param is a number greater than 1
        - return response if the check don't pass
          - return
            - status of 400
            - message of error from schema check
    
      ## how
      - use `zod` to validate the params
        - build a schema to perform checks
      - check
        - https://giancarlobuomprisco.com/next/protect-next-api-zod
    
      ## why
      - provide basic checks to prevent major errors (ex: params contains a
        non number)
    
      ## where
      - ./src/app/api/problems/[problemNum]/route.ts
    
      ## usage

--- src/app/api/problems/[problemNum]/route.ts
+++ src/app/api/problems/[problemNum]/route.ts
@@ -1,3 +1,6 @@
+import { NextResponse } from "next/server";
+import { z } from "zod";
+
 import { uhuntProblemNumUrl } from "@/utils/constants";
 
 type getParamsType = {
@@ -7,6 +10,23 @@ type getParamsType = {
 };
 
 export const GET = async (_request: Request, { params }: getParamsType) => {
+  const schema = z.object({
+    problemNum: z.coerce
+      .number({ invalid_type_error: "Must be a number" })
+      .min(1, "Must be a number greater than 0"),
+  });
+
+  const schemaResponse = await schema.safeParseAsync(params);
+  if (!schemaResponse.success) {
+    const message = {
+      message: schemaResponse.error.issues[0].message,
+    };
+
+    return NextResponse.json(message, {
+      status: 400,
+    });
+  }
+
   const { problemNum } = params;
 
   const url = uhuntProblemNumUrl(problemNum);
