c463a36ec9c9eb94a72285d143112ff126102f80
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2023-12-27 17:34:56 -0700

    chore(api): mutate the data returned from upstream for endpoint `/api/poll`
      ## what
      - mutate the data returned from upstream for endpoint `/api/poll`
        - convert `verdict ID` to a string
          - use object to map through the IDs
        - convert `language ID` to a string
          - use object to map through the IDs
        - convert `rank` to a string
          - return '-' if rank below 0.
          - return the provided value if rank is above 0
        - add problem number as an object property
        - add problem title as an object property
    
      ## how
      - map through each element in the array
    
      ## why
      - better to do preprocessing server side than client side
        - ex: getting the `problem number` from `problem id`
    
      ## where
      - ./src/app/api/poll/route.ts
    
      ## usage

--- src/app/api/poll/route.ts
+++ src/app/api/poll/route.ts
@@ -1,10 +1,32 @@
-import { fetchLiveSubmissionsUrl } from "@/utils/constants"
+import { Language, Problem, Submission, Verdict } from "@/types";
+import { fetchLiveSubmissionsUrl, uhuntProblemIdUrl } from "@/utils/constants";
 
 export const GET = async () => {
-  const url = fetchLiveSubmissionsUrl()
+  const url = fetchLiveSubmissionsUrl();
 
-  const response = await fetch(url)
-  const data = await response.json();
+  const fetchResponse = await fetch(url);
+  const data: Submission[] = await fetchResponse.json();
 
-  return Response.json(data)
-}
+  const converted = data.map(async (submission: Submission) => {
+    submission.msg.ver = Verdict[submission.msg.ver];
+    submission.msg.lan = Language[submission.msg.lan];
+
+    // add problem number to the object
+    const pidUrl = uhuntProblemIdUrl(`${submission.msg.pid}`);
+    const pidResponse = await fetch(pidUrl);
+    const pidData: Problem = await pidResponse.json();
+    submission.msg.pnum = pidData.num;
+    submission.msg.pTitle = pidData.title;
+
+    submission.msg.rank = +submission.msg.rank < 0 ? '-' : submission.msg.rank
+
+    return submission;
+  }).reverse();
+
+  // using Promise.all because the map function is using an async function
+  // use Promise.all to resolver async operations
+  // obtained from: https://stackoverflow.com/a/68279968/3053548
+  const response = await Promise.all(converted);
+
+  return Response.json(response);
+};
