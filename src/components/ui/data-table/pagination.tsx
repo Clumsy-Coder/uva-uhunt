0cc56e5f499428fbcb82e433615ba0458e75f67b
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-01-02 12:36:12 -0700

    chore(shadcn:data-table): display total number of items in the DataTable
      ## what
      - display total number of items in the DataTable
    
      ## how
    
      ## why
      - to give the user an idea how many items are there in a table
    
      ## where
      - ./src/components/ui/data-table/pagination.tsx
    
      ## usage

--- src/components/ui/data-table/pagination.tsx
+++ src/components/ui/data-table/pagination.tsx
@@ -24,10 +24,10 @@ export function DataTablePagination<TData>({
 }: DataTablePaginationProps<TData>) {
   return (
     <div className="flex items-center justify-end space-x-2 py-4">
-      {/* <div className="flex-1 text-sm text-muted-foreground"> */}
-      {/*   {table.getFilteredSelectedRowModel().rows.length} of{" "} */}
-      {/*   {table.getFilteredRowModel().rows.length} row(s) selected. */}
-      {/* </div> */}
+      <div className="flex-1 text-sm text-muted-foreground">
+        {/* {table.getFilteredSelectedRowModel().rows.length} of{" "} */}
+        Total <span className="font-bold">{table.getFilteredRowModel().rows.length}</span> item(s).
+      </div>
       <div className="flex items-center space-x-6 lg:space-x-8">
         <div className="flex items-center space-x-2">
           <p className="text-sm font-medium mr-1">Rows per page</p>
