9ecd8fddc09e0f63acaaa538dcbf0f8e22a1881a
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-01-03 08:21:38 -0700

    chore(components:data-table): add `columns.tsx` for home page
      ## what
      - add `columns.tsx` for home page
      - display the `Submission ID` column
    
      ## how
    
      ## why
      - this will be used to display live submissions on the home page
    
      ## where
      - ./src/app/components/data-table/columns.tsx
    
      ## usage

---
+++ src/app/components/data-table/columns.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import { ColumnDef } from "@tanstack/react-table"
+import Link from "next/link";
+
+import { buttonVariants } from "@/components/ui/button";
+import { DataTableColumnHeader } from "@/components/ui/data-table/column-header";
+import { Submission } from "@/types"
+
+export const columns: ColumnDef<Submission>[] = [
+  {
+    accessorKey: "submissionId",
+    accessorFn: row => row.msg.sid,
+    // meta: {
+    //   headerTitle: "Submission ID"
+    // },
+    header: ({ column }) => {
+      return (
+        <DataTableColumnHeader column={column} title="Submission ID" />
+      );
+    },
+    cell: ({ row }) => {
+      return (
+        <Link href={`/submissions/${row.getValue("submissionId")}`} className={buttonVariants({variant: 'outline'})}>
+          {row.getValue("submissionId")}
+        </Link>
+      );
+    },
+  },
+]
+
