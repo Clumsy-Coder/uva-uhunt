7a34d423162bd092926b3fc9b75f63d924e4e1d4
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-01-04 17:34:31 -0700

    refactor: move schema validation for endpoint `/api/problems/[problemNum]` to it's own file
      ## what
      - move schema validation for endpoint `/api/problems/[problemNum]` to it's own file
      - add schema in `/api/problems/[problemNum]` via import
    
      ## how
    
      ## why
      - the schema validation will also be used in the client side
        - used in `/problems[problemNum]`
    
      ## where
      - ./src/app/api/problems/[problemNum]/route.ts
      - ./src/schema/index.ts
    
      ## usage

---
+++ src/schema/index.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+/**
+ * Schema validation for endpoint `/api/problems/[problemNum]`
+ */
+export const problemNumSchema = z.object({
+  problemNum: z.coerce
+    .number({ invalid_type_error: "Problem number must be a number" })
+    .min(1, "Problem number must be a number greater than 0"),
+});
