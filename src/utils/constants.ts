f7c092db8559cffc606ea1230756a23f38af13bc
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-01-04 17:48:28 -0700

    refactor(utils:constants): add type `number` to uhunt urls
      ## what
      - add type `number` to uhunt urls
    
      ## how
    
      ## why
      - it's a little tedious to convert number to a string when using the
        function
    
      ## where
      - ./src/utils/constants.ts
    
      ## usage

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -15,9 +15,9 @@ export const viewProblemUrl =
 
 /**
  * URL for viewing the details of the problem PDF using problem ID
- * @param {String} pid problem ID
+ * @param {String | Number} pid problem ID
  */
-export const uhuntViewProblemUrl = (pid: string) => `${viewProblemUrl}${pid}`;
+export const uhuntViewProblemUrl = (pid: string | number) => `${viewProblemUrl}${pid}`;
 
 /**
  * URL for fetching all problems on uva-uhunt
@@ -26,25 +26,25 @@ export const uhuntAllProblemsUrl = () => 'https://uhunt.onlinejudge.org/api/p';
 
 /**
  * URL for getting Problem data using Problem ID
- * @param {String} pid - problem ID
+ * @param {String | Number} pid - problem ID
  */
-export const uhuntProblemIdUrl = (pid: string) =>
+export const uhuntProblemIdUrl = (pid: string | number) =>
   `${uhuntBaseApiUrl}/p/id/${pid}`;
 
 /**
  * URL for getting Problem data using Problem number
- * @param {String} problemNumber - problem number
+ * @param {String | Number} problemNumber - problem number
  */
-export const uhuntProblemNumUrl = (problemNumber: string) =>
+export const uhuntProblemNumUrl = (problemNumber: string | number) =>
   `${uhuntBaseApiUrl}/p/num/${problemNumber}`;
 
 /**
  * URL for getting Problem ranklist using Problem ID, starting rank and number of ranks to return
- * @param {String} pid - Problem ID
+ * @param {String | Number} pid - Problem ID
  * @param {Number} [start=1] - rank to start from. Default is 1
  * @param {Number} [count=100] - rank to go up to. Default is 100
  */
-export const uhuntProblemRankUrl = (pid: string, start = 1, count = 100) =>
+export const uhuntProblemRankUrl = (pid: string | number, start = 1, count = 100) =>
   `${uhuntBaseApiUrl}/p/rank/${pid}/${start}/${count}`;
 
 /**
@@ -53,13 +53,13 @@ export const uhuntProblemRankUrl = (pid: string, start = 1, count = 100) =>
  * represented in 20 array elements, each element represents 12 months.
  * The submissionTime represents at what time should look back from,
  * could be start from 5 years ago or now.
- * @param {String} pid - problem ID
+ * @param {String | Number} pid - problem ID
  * @param {Number} [submissionTime=moment().unix()] - Unix timestamp. Default is current unix timestamp
  * @param {Number} [back=20] - Number of years to look back. Default is 20
  * @param {Number} [jump=12] - Number of months each array element will represent. Default is 12
  */
 export const uhuntSubmissionCountUrl = (
-  pid: string,
+  pid: string | number,
   submissionTime = moment().unix(),
   back = 20,
   jump = 12,
@@ -74,16 +74,16 @@ export const uhuntUsername2UidUrl = (username: string) =>
 
 /**
  * URL for getting Submission list of a problem using Problem ID
- * @param {String} pid - problem ID
+ * @param {String | Number} pid - problem ID
  */
-export const uhuntProblemSubmissionListUrl = (pid: string) =>
+export const uhuntProblemSubmissionListUrl = (pid: string | number) =>
   `${uhuntBaseApiUrl}/p/subs/${pid}/0/${moment().unix()}`;
 
 /**
  * Get User submissions using UserID
- * @param {Number} uid User ID
+ * @param {String | Number} uid User ID
  */
-export const uhuntUserSubmissionsUrl = (uid: string) =>
+export const uhuntUserSubmissionsUrl = (uid: string | number) =>
   `${uhuntBaseApiUrl}/subs-user/${uid}`;
 
 /**
