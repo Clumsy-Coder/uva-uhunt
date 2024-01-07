2c90582c0b5c36ab67d1e37107379f6bf60b80f5
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-01-07 10:42:34 -0700

    refactor(types): combine multiple instances of `Verdict` into one
      ## what
      - combine multiple instances of `Verdict` into one
        - add type `ProblemVerdictType`
          - title: string to display in client side. usually for displaying tooltip title
          - bgColor: tailwindcss background color
          - fgColor: tailwindcss foreground color
          - bgHex: hexcode of property `bgColor`
          - fgHex: hexcode of propetty `fgColor`
        - apply type `ProblemVerdictType` in object `ProblemVerdictMap`
          - set their properties
          - set `Problem.ver` number as key into object `ProblemVerdictMap`
            - they map to the same key in the object `ProblemVerdictMap
            - doing this way to prevent duplocate code`
        - remove `VerdictType`
        - remove `Verdict`
          - the keys of this object are applied in object `ProblemVerdictMap`
        - set property `Submission.verdict` to type `ProblemVerdictType`
      - apply changes to other files
        - replace `Verdict` with `ProblemVerdictMap` in api endpoint
          `/api/poll[pollId]`
    
      ## how
    
      ## why
      - this to remove duplicate code
      - to combine string abbreviation of the verdict into object `ProblemVerdictMap`
      - to combine verdict ID from `Submission` into object `ProblemVerdictMap`
      - refactoring these later would be difficult
    
      ## where
      - ./src/app/api/poll/[pollId]/route.ts
      - ./src/types/index.ts
    
      ## usage

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,56 +4,179 @@ export const ProblemStatus: Record<string, string> = {
   2: "Special judge",
 };
 
-export const ProblemVerdictMap: Record<string, string> = {
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Problem verdict data structure
+ */
+export interface ProblemVerdictType {
+  /**
+   * Title to be displayed on the front end
+   */
+  title: string;
+  /**
+   * Tailwindcss for background color
+   * Usually used for displaying verdict using shadcn-ui `Badge` component
+   */
+  bgColor: string;
+  /**
+   * Tailwindcss for foreground color
+   * Usually used for displaying verdict using shadcn-ui `Badge` component
+   */
+  fgColor: string;
+  /**
+   * Hexcode of property `bgColor`
+   * Usually used for displaying verdict using shadcn-ui `Badge` component
+   */
+  bgHex: string;
+  /**
+   * Hexcode of property `fgColor`
+   * Usually used for displaying verdict using shadcn-ui `Badge` component
+   */
+  fgHex: string;
+}
+
+const ProblemVerdictMap: Record<string, ProblemVerdictType> = {
   /**
    * Number of Accepted
    */
-  ac: "Accepted",
+  ac: {
+    title: "Accepted",
+    bgColor: "bg-[#00AA00]",
+    fgColor: "text-primary-foreground dark:text-secondary-foreground",
+    bgHex: "#00AA00",
+    fgHex: "",
+  },
   /**
    * Number of Presentation Error
    */
-  pe: "Presentation error",
+  pe: {
+    title: "Presentation error",
+    bgColor: "bg-[#666600]",
+    fgColor: "text-primary-foreground dark:text-secondary-foreground",
+    bgHex: "#666600",
+    fgHex: "",
+  },
   /**
    * Number of Wrong Answer
    */
-  wa: "Wrong answer",
+  wa: {
+    title: "Wrong answer",
+    bgColor: "bg-[#FF0000]",
+    fgColor: "text-primary-foreground dark:text-secondary-foreground",
+    bgHex: "#FF0000",
+    fgHex: "",
+  },
   /**
    * Number of Time Limit Exceeded
    */
-  tle: "Time limit exceeded",
+  tle: {
+    title: "Time limit exceeded",
+    bgColor: "bg-[#0000FF]",
+    fgColor: "text-primary-foreground dark:text-secondary-foreground",
+    bgHex: "#0000FF",
+    fgHex: "",
+  },
   /**
    * Number of Memory Limit Exceeded
    */
-  mle: "Memory limit exceeded",
+  mle: {
+    title: "Memory limit exceeded",
+    bgColor: "bg-[#0000AA]",
+    fgColor: "text-primary-foreground dark:text-secondary-foreground",
+    bgHex: "#0000AA",
+    fgHex: "",
+  },
   /**
    * Number of Compilation Error
    */
-  ce: "Compile error",
+  ce: {
+    title: "Compile error",
+    bgColor: "bg-orange-600",
+    fgColor: "text-primary-foreground dark:text-secondary-foreground",
+    bgHex: "#EA5A0C",
+    fgHex: "",
+  },
   /**
    * Number of Runtime Error
    */
-  re: "Runtime error",
+  re: {
+    title: "Runtime error",
+    bgColor: "bg-[#00AAAA]",
+    fgColor: "text-primary-foreground dark:text-secondary-foreground",
+    bgHex: "#00AAAA",
+    fgHex: "",
+  },
   /**
    * Number of Output Limit Exceeded
    */
-  ole: "Output limit exceeded",
+  ole: {
+    title: "Output limit exceeded",
+    bgColor: "bg-[#000066]",
+    fgColor: "text-primary-foreground dark:text-secondary-foreground",
+    bgHex: "#000066",
+    fgHex: "",
+  },
   /**
    * Number of Submission Error
    */
-  // sube: "Submission Error",
+  sube: {
+    title: "Submission Error",
+    bgColor: "bg-gray-500",
+    fgColor: "text-primary-foreground dark:text-secondary-foreground",
+    bgHex: "6b7280",
+    fgHex: "",
+  },
   /**
    * Number of Can't be Judged
    */
-  // noj: "Can't be judged",
+  noj: {
+    title: "Can't be judged",
+    bgColor: "bg-gray-500",
+    fgColor: "text-primary-foreground dark:text-secondary-foreground",
+    bgHex: "6b7280",
+    fgHex: "",
+  },
   /**
    * Number of In Queue
    */
-  // inq: number,
+  inq: {
+    title: "- In Queue -",
+    bgColor: "bg-gray-500",
+    fgColor: "text-primary-foreground dark:text-secondary-foreground",
+    bgHex: "6b7280",
+    fgHex: "",
+  },
   /**
    * Number of Restricted Function
    */
-  // rf: "Restricted function",
+  rf: {
+    title: "Restricted function",
+    bgColor: "bg-gray-500",
+    fgColor: "text-primary-foreground dark:text-secondary-foreground",
+    bgHex: "6b7280",
+    fgHex: "",
+  }
 }
+// adding keys from `Submission.ver` into object `ProblemVerdictMap`
+// will reduce the duplicate code
+// these verdict IDs have the same styling properties
+ProblemVerdictMap["0"]  = {...ProblemVerdictMap.inq  };
+ProblemVerdictMap["10"] = {...ProblemVerdictMap.sube };
+ProblemVerdictMap["15"] = {...ProblemVerdictMap.noj  };
+ProblemVerdictMap["20"] = {...ProblemVerdictMap.inq  };
+ProblemVerdictMap["30"] = {...ProblemVerdictMap.ce   };
+ProblemVerdictMap["35"] = {...ProblemVerdictMap.rf   };
+ProblemVerdictMap["40"] = {...ProblemVerdictMap.re   };
+ProblemVerdictMap["45"] = {...ProblemVerdictMap.ole  };
+ProblemVerdictMap["50"] = {...ProblemVerdictMap.tle  };
+ProblemVerdictMap["60"] = {...ProblemVerdictMap.mle  };
+ProblemVerdictMap["70"] = {...ProblemVerdictMap.wa   };
+ProblemVerdictMap["80"] = {...ProblemVerdictMap.pe   };
+ProblemVerdictMap["90"] = {...ProblemVerdictMap.ac   };
+export { ProblemVerdictMap };
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
 /**
  * Data structure returned when querying from url `https://uhunt.onlinejudge.org/api/p/num/:num`
@@ -148,36 +271,7 @@ export type Problem = {
   rej: number;
 };
 
-export type VerdictType = {
-  /**
-   * Tailwindcss styles for foreground
-   */
-  fgColor: string;
-  /**
-   * Tailwindcss styles for background
-   */
-  bgColor: string;
-  /**
-   * Title to be displayed on the front end
-   */
-  title: string;
-};
-
-export const Verdict: Record<string, VerdictType> = {
-  0 : { fgColor: "text-primary-foreground dark:text-secondary-foreground", bgColor: "bg-gray-500",    title: "- In Queue -"         },
-  10: { fgColor: "text-primary-foreground dark:text-secondary-foreground", bgColor: "bg-gray-500",    title: "Submission error"     },
-  15: { fgColor: "text-primary-foreground dark:text-secondary-foreground", bgColor: "bg-gray-500",    title: "Can't be judged"      },
-  20: { fgColor: "text-primary-foreground dark:text-secondary-foreground", bgColor: "bg-gray-500",    title: "- In Queue -"         },
-  30: { fgColor: "text-primary-foreground dark:text-secondary-foreground", bgColor: "bg-orange-600",  title: "Compile error"        },
-  35: { fgColor: "text-primary-foreground dark:text-secondary-foreground", bgColor: "bg-gray-500",    title: "Restricted function"  },
-  40: { fgColor: "text-primary-foreground dark:text-secondary-foreground", bgColor: "bg-[#00AAAA]",   title: "Runtime error"        },
-  45: { fgColor: "text-primary-foreground dark:text-secondary-foreground", bgColor: "bg-[#000066]",   title: "Output limit"         },
-  50: { fgColor: "text-primary-foreground dark:text-secondary-foreground", bgColor: "bg-[#0000FF]",   title: "Time limit"           },
-  60: { fgColor: "text-primary-foreground dark:text-secondary-foreground", bgColor: "bg-[#0000AA]",   title: "Memory limit"         },
-  70: { fgColor: "text-primary-foreground dark:text-secondary-foreground", bgColor: "bg-[#FF0000]",   title: "Wrong answer"         },
-  80: { fgColor: "text-primary-foreground dark:text-secondary-foreground", bgColor: "bg-[#666600]",   title: "Presentation error"   },
-  90: { fgColor: "text-primary-foreground dark:text-secondary-foreground", bgColor: "bg-[#00AA00]",   title: "Accepted"             },
-};
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
 export const Language: Record<string, string> = {
   1: "ANSI C",
@@ -186,6 +280,8 @@ export const Language: Record<string, string> = {
   4: "Pascal",
   5: "C++11",
 };
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
 /**
  * Submission data structure
@@ -227,15 +323,13 @@ export type Submission = {
      * Verdict properties
      *
      * contains 
-     * - verdict title Ex: `Accepted`, or `Compile error`
+     * - title: verdict title Ex: `Accepted`, or `Compile error`
      * - fgColor: tailwindcss class to use for foreground color
      * - bgColor: tailwindcss class to use for background color
+     * - fgHex: hexcode of the property `fgColor`
+     * - bgHex: hexcode of property `bgColor`
      */
-    verdict: {
-      title: string;
-      fgColor: string;
-      bgColor: string;
-    };
+    verdict: ProblemVerdictType;
     /**
      * Language ID
      */
@@ -266,3 +360,6 @@ export type Submission = {
     uname: string;
   };
 };
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
