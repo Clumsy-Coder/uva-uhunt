f853c3c8f63c4074b71cf6669a0d1eabfc09b934
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-01-08 17:50:54 -0700

    feat(api:submission:language): add endpoint `/api/submissions/language/[problemNum]`
      ## what
      - add endpoint `/api/submissions/language/[problemNum]`
        - get submissions language count of a problem using problem number
          - if invalid `problem number` is given, a response of 400 will be returned
          - if the problem doesn't exist, a response of 404 will be returned
          - fetch submissions of problem number
          - count submission language
    
      ## how
    
      ## why
    
      ## where
      - ./src/app/api/submissions/language/[problemNum]/route.tsx
    
      ## usage

---
+++ src/app/api/submissions/language/[problemNum]/route.tsx
@@ -0,0 +1,76 @@
+import { NextResponse } from "next/server";
+import { z } from "zod";
+
+import { submissionLangSchema as schema } from "@/schema";
+import {
+  uhuntProblemNumUrl,
+  uhuntProblemSubmissionListUrl,
+} from "@/utils/constants";
+import { Language, Problem, Submission } from "@/types";
+
+type getParamsType = {
+  params: z.infer<typeof schema>;
+};
+
+export type getResponseType = Record<string, number>;
+
+export const GET = async (_request: Request, { params }: getParamsType) => {
+  // validate params
+  const schemaResponse = await schema.safeParseAsync(params);
+  if (!schemaResponse.success) {
+    const message = {
+      message: schemaResponse.error.issues[0].message,
+    };
+
+    return NextResponse.json(message, {
+      status: 400,
+    });
+  }
+
+  //----------------------------------------------------------------------------------------------//
+
+  // fetch problem stats
+  const { problemNum } = params;
+
+  const problemUrl = uhuntProblemNumUrl(problemNum);
+  const problemResponse = await fetch(problemUrl);
+  const problemData: Problem = await problemResponse.json();
+
+  // return 404 if problem doesn't exist
+  if (Object.entries(problemData).length === 0) {
+    const message = {
+      message: `Problem number ${problemNum} not found`,
+    };
+    return NextResponse.json(message, {
+      status: 404,
+    });
+  }
+
+  //----------------------------------------------------------------------------------------------//
+
+  // fetch submissions of the problem
+  const submissionsUrl = uhuntProblemSubmissionListUrl(problemData.pid);
+  const submissionResponse = await fetch(submissionsUrl, { cache: "no-cache" });
+  const submissionData: Submission["msg"][] = await submissionResponse.json();
+
+  // map language id as key and 0 as value. (the value will be count of a submission language)
+  const languageObj = Object.keys(Language).reduce(
+    (acc: Record<string, number>, cur: string) => {
+      acc[cur] = 0;
+
+      return acc;
+    },
+    {},
+  );
+
+  // increment count of key-value for their respective language ID
+  const responseData: getResponseType = submissionData.reduce((acc, cur) => {
+    const languageId = cur.lan;
+    acc[languageId] = acc[languageId] + 1;
+
+    return acc;
+  }, languageObj);
+  delete responseData["undefined"];
+
+  return Response.json(responseData);
+};
